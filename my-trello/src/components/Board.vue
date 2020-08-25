943a09c35c79db109987d9de5e5cbf208e046ae3
kottyan <exm6969@gmail.com>
2020-08-26 01:33:28 +0900

    section-5 part.3

--- my-trello/src/components/Board.vue
+++ my-trello/src/components/Board.vue
@@ -6,6 +6,9 @@
     <main>
       <p class="info-line">All: {{ totalCardCount }} tasks</p>
       <div class="list-index">
+      <draggable :list="lists"
+                  @end="movingList"
+                  class="list-index">
         <List v-for="(item, index) in lists"
               :key="item.id"
               :title="item.title"
@@ -14,11 +17,13 @@
               @change="movingCard"
         />
         <ListAdd />
+      </draggable>
       </div>
     </main>
   </div>
 </template>
 <script>
+import draggable from 'vuedraggable'
 import ListAdd from './ListAdd.vue'
 import List from './List'
 import { mapState } from 'vuex'
@@ -26,7 +31,8 @@ import { mapState } from 'vuex'
 export default {
   components: {
     ListAdd,
-    List
+    List,
+    draggable
   },
   computed: {
     ...mapState([
@@ -40,6 +46,9 @@ export default {
     movingCard: function() {
       this.$store.dispatch('updateList', { lists: this.lists })
     },
+    movingList: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    }
   }
 }
 </script>
