88d71200bb3fe2d8e7e361a30316426ebd6aaef7
kottyan <exm6969@gmail.com>
2020-08-25 09:16:36 +0900

    section-2 part.1

---
+++ my-trello/src/components/Board.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header>
+      my Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+    </main>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
