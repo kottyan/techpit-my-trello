a17b2bc3a632f919896aa7911eab1576c6f5f4df
kottyan <exm6969@gmail.com>
2020-08-25 23:55:59 +0900

    section-3 part.2

--- my-trello/src/components/ListAdd.vue
+++ my-trello/src/components/ListAdd.vue
@@ -1,11 +1,15 @@
 <template>
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classList" @submit.prevent="addList">
     <input v-model="title"
            type="text"
            class="text-input"
            placeholder="Add new list"
+           @focusin="startEditing"
+           @focusout="finishEditing"
     >
-    <button type="submit" class="add-button">
+    <button type="submit"
+      class="add-button"
+      v-if="isEditing || titleExists">
       Add
     </button>
   </form>
@@ -15,12 +19,34 @@ export default {
   data: function() {
     return {
       title: '',
+      isEditing: false,
     }
+  },
+  computed: {
+    classList() {
+      const classList = ['addlist']
+      if (this.isEditing) {
+        classList.push('active')
+      }
+      if (this.titleExists) {
+        classList.push('addable')
+      }
+      return classList
+    },
+    titleExists() {
+      return this.title.length > 0
+    },
   },
   methods: {
     addList: function() {
       this.$store.dispatch('addlist', { title: this.title })
       this.title = ''
+    },
+    startEditing() {
+      this.isEditing = true
+    },
+    finishEditing() {
+      this.isEditing = false
     },
   }
 }
